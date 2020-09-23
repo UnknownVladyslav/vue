252bfb147fba8402e0a02a198183ddfba1dbdee4
UnknownVladyslav <67956984+UnknownVladyslav@users.noreply.github.com>
2020-09-23 22:28:20 +0300

    Training files added

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let app = new Vue ({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'Common green high quality socks',
+        image: 'img/socks_img.jpg',
+        link: 'https://google.com.ua',
+        inStock: true,
+        inventory: 10
+    }
+})
+
